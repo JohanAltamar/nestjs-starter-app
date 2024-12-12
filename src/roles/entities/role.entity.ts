32dad9a6908af8b305ffbf930da2709b6b25f069
Johan Altamar <johan.altamar.dev@gmail.com>
2024-12-12 17:01:46 -0500

    Add: Role entity in db

--- src/roles/entities/role.entity.ts
+++ src/roles/entities/role.entity.ts
@@ -1,1 +1,18 @@
-export class Role {}
+import { BeforeInsert, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'roles' })
+export class Role {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('text', { unique: true })
+  name: string;
+
+  @Column('text', { nullable: true })
+  description: string;
+
+  @BeforeInsert()
+  beforeInsertRole() {
+    this.name = this.name.toUpperCase();
+  }
+}
--- src/roles/roles.module.ts
+++ src/roles/roles.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
-import { RolesService } from './roles.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+// Controllers
 import { RolesController } from './roles.controller';
+
+// Entities
+import { Role } from './entities/role.entity';
+
+// Providers
+import { RolesService } from './roles.service';
 
 @Module({
   controllers: [RolesController],
   providers: [RolesService],
+  imports: [TypeOrmModule.forFeature([Role])],
 })
 export class RolesModule {}
